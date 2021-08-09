fd013f0d9843b695a449cd366f8247c412f03ba4
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2021-08-09 16:21:51 +0800

    solutions for 39 combinationSum

---
+++ 39 combinationSum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        backtrack(list, new ArrayList<>(), candidates, target,  0);
+        return list;
+    }
+
+    public void backtrack(List<List<Integer>> list, List<Integer> comb, int[] nums, int remain, int start)
+    {
+        if(remain < 0)
+            return;
+
+        if(remain == 0)
+            list.add(new ArrayList<>(comb));
+
+        else
+        {
+            for(int i = start; i < nums.length; i++)
+            {
+                comb.add(nums[i]);
+                backtrack(list, comb, nums, remain - nums[i], i);
+                comb.remove(comb.size() - 1);
+            }
+        }
+    }
+}
