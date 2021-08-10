9e749a69c7d2a8e4236c054657453f208cd577f2
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2021-08-10 16:01:40 +0800

    solutions for Offer 53 -II missingNumber

---
+++ 53 -II missingNumber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == mid)
+                left = mid + 1;
+
+            else if(nums[mid] > mid)
+                right = mid - 1;
+        }
+
+        return left;
+    }
+}
