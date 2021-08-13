cfd1d3cf3f3a12c9104276d3c067d7a1d1053e87
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2021-08-13 10:11:11 +0800

    solutions for 24 swapPairs

---
+++ 24 swapPairs.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode temp = dummy;
+        
+        while(temp.next != null && temp.next.next != null)
+        {
+            ListNode start = temp.next;
+            ListNode end = temp.next.next;
+            temp.next = end;
+            start.next = end.next;
+            end.next = start;
+            temp = start;
+        }
+
+        return dummy.next;
+    }
+}
