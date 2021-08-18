9dcd05c7e7fb9f8db0b2f44101f230e9760aeeb0
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2021-08-18 14:18:51 +0800

    solutions for Offer 25 mergeTwoLists

---
+++ Offer 25 mergeTwoLists.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode ptr = dummy;
+        while(l1 != null && l2 != null)
+        {
+            if(l1.val <= l2.val)
+            {
+                dummy.next = l1;
+                dummy = dummy.next;
+                l1 = l1.next;
+            }
+
+            else
+            {
+                dummy.next = l2;
+                dummy = dummy.next;
+                l2 = l2.next;
+            }
+        }
+
+        ListNode cur = l1 == null ? l2 : l1;
+
+        while(cur != null)
+        {
+            dummy.next = cur;
+            dummy = dummy.next;
+            cur = cur.next;
+        }
+
+        return ptr.next;
+    }
+}
