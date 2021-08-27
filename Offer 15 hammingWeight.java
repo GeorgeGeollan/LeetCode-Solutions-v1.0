23c49fc0ddca8a788fd5bf9e367839fa1dd5dfc1
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2021-08-27 20:13:58 +0800

    solutions for Offer 15 hammingWeight

--- Offer 15 hammingWeight.java
+++ Offer 15 hammingWeight.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution1 {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
         int count = 0;
@@ -8,6 +8,20 @@ public class Solution {
             count += n & 1;
 
             n >>>= 1;
+        }
+
+        return count;
+    }
+}
+
+public class Solution2 {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        while(n != 0)
+        {
+            count++;
+            n &= (n - 1);
         }
 
         return count;
