0cbe2900be216348f8deb54ac6d4fd4ee9721c7b
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2021-08-28 09:27:43 +0800

    solutions for Offer 56 -I singleNumbers

---
+++ Offer 56 -I singleNumbers.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] singleNumbers(int[] nums) {
+        int m = 1, n = 0, x = 0, y = 0;
+
+        for(int num: nums)
+            n ^= num;
+
+        while((m & n) == 0)
+            m <<= 1;
+        
+        for(int num: nums)
+        {
+            if((num & m) == 0)
+                x ^= num;
+            
+            else
+                y ^= num;
+        }
+
+        return new int[]{x, y};
+
+    }
+}
