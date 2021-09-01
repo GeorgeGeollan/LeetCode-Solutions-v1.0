7585469d80272edc7ecfd99f070088aab0d03f09
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2021-09-01 09:29:30 +0800

    solutions for 153 findMin

---
+++ 153 findMin.java
@@ -0,0 +1,36 @@
+class Solution1 {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right)
+        {
+            if(nums[left] <= nums[right])
+                return nums[left];
+            
+            else if(nums[left] > nums[right])
+                left++;
+        }
+
+        return -1;
+    }
+}
+
+class Solution2 {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = 0;
+
+        while(left < right)
+        {
+            mid = left + (right - left) / 2;
+            if(nums[mid] > nums[right])
+                left = mid + 1;
+            else if(nums[mid] <= nums[right])
+                right = mid;
+        }
+
+        return nums[left];
+    }
+}
