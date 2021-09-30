59695e6827c0297e696b7d1fe7f790ccf199cefb
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2021-09-30 10:59:35 +0800

    solutions for 223 computeArea

---
+++ 223 computeArea.java
@@ -0,0 +1,31 @@
+class Solution1 {
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int area1 = (ax2 - ax1) * (ay2 - ay1);
+        int area2 = (bx2 - bx1) * (by2 - by1);
+
+        if(ax2 <= bx1 || bx2 <= ax1 || ay2 <= by1 || by2 <= ay1)
+            return area1 + area2;
+
+        int cx1 = Math.max(ax1, bx1);
+        int cx2 = Math.min(ax2, bx2);
+
+        int cy1 = Math.max(ay1, by1);
+        int cy2 = Math.min(ay2, by2);
+        int area3 = (cx2 - cx1) * (cy2 - cy1);
+
+        return area1 + area2 - area3;
+    }
+}
+
+class Solution2 {
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int area1 = (ax2 - ax1) * (ay2 - ay1);
+        int area2 = (bx2 - bx1) * (by2 - by1);
+
+        int x = Math.max(0, Math.min(ax2, bx2) - Math.max(ax1, bx1));
+        int y = Math.max(0, Math.min(ay2, by2) - Math.max(ay1, by1));
+
+
+        return area1 + area2 - x * y;
+    }
+}
