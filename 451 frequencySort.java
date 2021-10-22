8a876aa900f89359a2aa69356d0c6c3e589613a8
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2021-10-22 14:06:11 +0800

    solutions for 451 frequencySort

--- 451 frequencySort.java
+++ 451 frequencySort.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public String frequencySort(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
         char[] words = s.toCharArray();
@@ -26,5 +26,38 @@ class Solution {
 
         return sb.toString();
 
+    }
+}
+
+class Solution2 {
+    public String frequencySort(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for(char letter: s.toCharArray())
+            map.put(letter, map.getOrDefault(letter, 0) + 1);
+
+        List<Character>[] buckets = new List[s.length() + 1];
+
+        for(char key: map.keySet()) {
+            int value = map.get(key);
+
+            if(buckets[value] == null)
+                buckets[value] = new ArrayList<Character>();
+
+            buckets[value].add(key);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = buckets.length - 1; i > 0; i--) {
+            if(buckets[i] != null) {
+                for(char letter: buckets[i]) {
+                    for(int j = i; j > 0; j--)
+                        sb.append(letter);
+                }
+            }
+        }
+
+        return sb.toString();
     }
 }
