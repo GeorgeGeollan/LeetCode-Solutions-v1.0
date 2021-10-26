eec9ad9f7dffd4416c7cfc1a290148c10ccefd2f
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2021-10-26 09:59:07 +0800

    solutions for 496 nextGreaterElement

--- 496 nextGreaterElement.java
+++ 496 nextGreaterElement.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         HashMap<Integer, Integer> map = new HashMap<>();
         int[] res = new int[nums1.length];
@@ -23,6 +23,29 @@ class Solution {
                 }
             }
         }
+
+        return res;
+    }
+}
+
+class Solution2 {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[nums1.length];
+
+        for(int i = nums2.length - 1; i >= 0; i--) {
+            int x = nums2[i];
+
+            while(!deque.isEmpty() && deque.peekLast() <= x)
+                deque.pollLast();
+
+            map.put(nums2[i], deque.isEmpty() ? -1 : deque.peekLast());
+            deque.addLast(nums2[i]);
+        }
+
+        for(int i = 0; i < nums1.length; i++)
+            res[i] = map.get(nums1[i]);
 
         return res;
     }
