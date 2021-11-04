91d71c83b03e077e16418aaf114b2e19e9c8a9f2
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2021-11-04 09:02:50 +0800

    solutions for 404 sumOfLeftLeaves

---
+++ 404 sumOfLeftLeaves.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum;
+    public int sumOfLeftLeaves(TreeNode root) {
+        sum = 0;
+        
+        dfs(root, 0);
+        
+        return sum;
+    }
+    
+    public void dfs(TreeNode root, int status) {
+        if(root == null)
+            return;
+        
+        if(status == 1 && root.left == null && root.right == null)
+            sum += root.val;
+        
+        dfs(root.left, 1);
+        dfs(root.right, 2);
+    }
+}
