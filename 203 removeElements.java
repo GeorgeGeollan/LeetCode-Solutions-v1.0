33ad403edfdf9ae6176936cf3b7dfe30970be33f
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2021-11-12 09:29:31 +0800

    solutions for 203 removeElements

---
+++ 203 removeElements.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode prev = dummy;
+        
+        while(head != null) {
+            if(head.val == val) {
+                prev.next = head.next;
+                head = head.next;
+            }
+            
+            else {
+                head = head.next;
+                prev = prev.next;
+            }
+        }
+        
+        return dummy.next;
+    }
+}
