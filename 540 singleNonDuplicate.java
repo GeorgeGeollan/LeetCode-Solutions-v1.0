d58f84c9a7a1dc10d80983162d81f9e8b3fe512f
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2021-11-20 12:15:20 +0800

    solutions for 540 singleNonDuplicate

---
+++ 540 singleNonDuplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int res = 0;
+        
+        for(int num: nums) {
+            res ^= num;
+        }
+        
+        return res;
+    }
+}
