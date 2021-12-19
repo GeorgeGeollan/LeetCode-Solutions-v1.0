055c26c67893253acf5e7e142bfb57f32efb40b2
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2021-12-19 11:45:15 +0800

    solutions for 997 findJudge

--- 997 findJudge.java
+++ 997 findJudge.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int findJudge(int n, int[][] trust) {
         if(n == 1)
             return 1;
@@ -16,5 +16,33 @@ class Solution {
 
 
         return -1;
+    }
+}
+
+class Solution2 {
+    public int findJudge(int n, int[][] trust) {
+        int[] inDegree = new int[n + 1];
+        int[] outDegree = new int[n + 1];
+        int len = trust.length;
+
+        int judgeIdx = 0;
+
+        for(int i = 0; i < len; i++) {
+            outDegree[trust[i][0]]++;
+            inDegree[trust[i][1]]++;
+        }
+
+        for(int i = 1; i <= n; i++) {
+            if(outDegree[i] == 0) {
+                judgeIdx = i;
+                break;
+            }
+        }
+
+        if(inDegree[judgeIdx] == n - 1)
+            return judgeIdx;
+
+        else
+            return -1;
     }
 }
