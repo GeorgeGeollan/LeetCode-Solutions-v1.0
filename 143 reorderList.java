123af127d22f94cecfb4254ce7da4e568a9b6d22
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2021-12-22 09:29:22 +0800

    solutions for 143 reorderList

--- 143 reorderList.java
+++ 143 reorderList.java
@@ -8,7 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+class Solution1 {
     public void reorderList(ListNode head) {
           if(head == null || head.next == null)
             return;
@@ -45,5 +45,53 @@ class Solution {
             p2 = pre.next;
         }
 
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution2 {
+    public void reorderList(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        ListNode reverse = reverseList(slow);
+        
+    
+        while(head.next != null && reverse.next != null) {
+            ListNode tmp1=  head.next;
+            ListNode tmp2 = reverse.next;
+            head.next = reverse;
+            reverse.next = tmp1;
+            head = tmp1;
+            reverse = tmp2;
+        }
+        
+    }
+    
+    public ListNode reverseList(ListNode root) {
+        ListNode dummy = null;
+        
+        while(root != null) {
+            ListNode next = root.next;
+            root.next = dummy;
+            dummy = root;
+            root = next;
+        }
+        
+        return dummy;
     }
 }
