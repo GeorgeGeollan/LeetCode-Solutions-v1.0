dc37fac64800b08d3c823e859988940936e728a5
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-01-01 10:08:22 +0800

    solutions for 2022 construct2DArray

---
+++ 2022 construct2DArray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        int len = original.length;
+        if(m * n != len)
+            return new int[0][0];
+
+        int[][] res = new int[m][n];
+
+        for(int i = 0; i < len; i++)
+            res[i / n][i % n] = original[i];
+
+        return res;
+    }
+}
