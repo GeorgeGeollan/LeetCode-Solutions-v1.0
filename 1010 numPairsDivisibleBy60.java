27b19bc02ded2fbcbe9c2575843d71e5d2357744
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-01-02 13:07:27 +0800

    solutions for 1010 numPairsDivisibleBy60

---
+++ 1010 numPairsDivisibleBy60.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numPairsDivisibleBy60(int[] time) {
+        int[] count = new int[60];
+        int ans = 0;
+        
+        for(int t: time)
+            count[t % 60]++;
+        
+        for(int i = 0; i <= 30; i++) {
+            if(i == 0 || i == 30) {
+                int cur = count[i];
+                ans += cur * (cur - 1) / 2;
+            }
+            
+            else
+                ans += count[i] * count[60 - i];
+        }
+        
+        return ans;
+    }
+}
