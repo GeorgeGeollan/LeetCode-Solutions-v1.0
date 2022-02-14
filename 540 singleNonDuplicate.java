519402c5d370502cd65a1238df7a30b66d5bc216
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-02-14 08:49:34 +0800

    solutinos for 540 singleNonDuplicate

--- 540 singleNonDuplicate.java
+++ 540 singleNonDuplicate.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int singleNonDuplicate(int[] nums) {
         int res = 0;
         
@@ -7,5 +7,30 @@ class Solution {
         }
         
         return res;
+    }
+}
+
+class Solution2 {
+    public int singleNonDuplicate(int[] nums) {
+       int n = nums.length;
+       int l = 0, r = n - 1;
+
+       while(l < r) {
+           int mid = l + r >> 1;
+
+           if(mid % 2 == 0) {
+               if(mid + 1 < n && nums[mid] == nums[mid + 1]) l = mid + 1;
+
+                else r = mid;
+           }
+
+           else {
+               if(mid - 1 >= 0 && nums[mid - 1] == nums[mid]) l = mid + 1;
+
+               else r = mid;
+           }
+       }
+
+       return nums[r];
     }
 }
