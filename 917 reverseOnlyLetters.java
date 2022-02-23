40b64193064a8a0ad4c1c395f9d96338f0726773
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-02-23 09:03:14 +0800

    solutions for 917 reverseOnlyLetters

--- 917 reverseOnlyLetters.java
+++ 917 reverseOnlyLetters.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public String reverseOnlyLetters(String s) {
         Stack<Character> stack = new Stack();
         char[] characters = s.toCharArray();
@@ -16,5 +16,31 @@ class Solution {
         }
 
         return String.valueOf(characters);
+    }
+}
+
+class Solution2 {
+    public String reverseOnlyLetters(String s) {
+        int n = s.length();
+        int left = 0, right = s.length() - 1;
+        char[] ch = s.toCharArray();
+
+        while(left < right) {
+            while(left < right && !Character.isLetter(ch[left])) {
+                left++;
+            }
+
+            while(left < right && !Character.isLetter(ch[right])) {
+                right--;
+            }
+
+            if(left <right) {
+                char temp = ch[left];
+                ch[left++] = ch[right];
+                ch[right--] = temp;
+            }
+        }
+
+        return String.valueOf(ch);
     }
 }
