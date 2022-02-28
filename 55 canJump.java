738a2256a3898b6552780a56f0e5422803634834
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-02-28 14:24:31 +0800

    solutions for 55 canJump

--- 55 canJump.java
+++ 55 canJump.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public boolean canJump(int[] nums) {
         int maxPosition = 0;
         int n = nums.length;
@@ -15,5 +15,21 @@ class Solution {
         }
         
         return true;
+    }
+}
+
+class Solution2 {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int cover = 0;
+
+        for(int i = 0; i <= cover; i++) {
+            cover = Math.max(nums[i] + i, cover);
+
+            if(cover >= n - 1)
+                return true;
+        }
+
+        return false;
     }
 }
