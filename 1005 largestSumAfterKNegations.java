a21c28e2fbc2b7be71847c0c1130fe1548af070b
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-03-03 15:48:56 +0800

    solutions for 1005 largestSumAfterKNegations

--- 1005 largestSumAfterKNegations.java
+++ 1005 largestSumAfterKNegations.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int largestSumAfterKNegations(int[] nums, int k) {
         int[] cnt = new int[210];
         for(int i: nums)
@@ -24,6 +24,33 @@ class Solution {
         int sum = 0;
         for(int i = -100; i <= 100; i++)
             sum += cnt[i + 100] * i;
+
+        return sum;
+    }
+}
+
+class Solution2 {
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        if(nums.length == 1)
+            return k % 2 == 0 ? nums[0] : -nums[0];
+        Arrays.sort(nums);
+        int sum = 0;
+        int idx = 0;
+
+        for(int i = 0; i < k; i++) {
+            if(i < nums.length - 1 && nums[idx] < 0) {
+                nums[idx] = -nums[idx];
+
+                if(nums[idx] >= Math.abs(nums[idx + 1]))
+                    idx++;
+                continue;
+            }
+
+            nums[idx] = -nums[idx];
+        }
+
+        for(int num: nums)
+            sum += num;
 
         return sum;
     }
