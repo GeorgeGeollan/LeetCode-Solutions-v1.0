9338ecb29be2463b648d615169716015712360bc
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-03-10 14:41:22 +0800

    solutions for 2 addTwoNumbers

--- 2 addTwoNumbers.java
+++ 2 addTwoNumbers.java
@@ -52,16 +52,6 @@ class Solution1 {
     }
 }
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution2 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode ans = new ListNode();
@@ -96,6 +86,37 @@ class Solution2 {
         
         if(impr == 1)
             ans.next = new ListNode(impr);
+        
+        return dummy.next;
+    }
+}
+
+class Solution3 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode ans = new ListNode();
+        ListNode dummy = ans;
+        int impr = 0;
+        int value = 0;
+        
+        while(l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            
+            int sum = x + y + impr;
+            impr = sum / 10;
+            sum = sum % 10;
+            ans.next = new ListNode(sum);
+            ans = ans.next;
+            
+            if(l1 != null)
+                l1 = l1.next;
+            
+            if(l2 != null)
+                l2 = l2.next;
+        }
+        
+        if(impr == 1)
+            ans.next = new ListNode(1);
         
         return dummy.next;
     }
