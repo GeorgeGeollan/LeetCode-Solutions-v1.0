4499974783a4a015048ce63f23703fdf6e911f51
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-03-10 14:35:24 +0800

    solutions for addTwoNumbers

--- 2 addTwoNumbers.java
+++ 2 addTwoNumbers.java
@@ -8,7 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+class Solution1 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int count = 0;
         ListNode n = new ListNode();
@@ -49,5 +49,54 @@ class Solution {
             n.next = new ListNode(count);
 
         return ptr.next;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode ans = new ListNode();
+        ListNode dummy = ans;
+        int impr = 0;
+        int value = 0;
+        
+        while(l1 != null || l2 != null) {
+            if(l1 != null && l2 != null) {
+                value = (l1.val + l2.val + impr) % 10;
+                impr = (l1.val + l2.val + impr)  / 10;
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+            
+            
+            else if(l1 == null) {
+                value = (l2.val + impr) % 10;
+                impr = (l2.val + impr) / 10;
+                l2 = l2.next;
+            }
+            
+            else if(l2 == null) {
+                value = (l1.val + impr) % 10;
+                impr = (l1.val + impr) / 10;
+                l1 = l1.next;
+            }
+            
+            ans.next = new ListNode(value);
+            ans = ans.next;
+        }
+        
+        if(impr == 1)
+            ans.next = new ListNode(impr);
+        
+        return dummy.next;
     }
 }
