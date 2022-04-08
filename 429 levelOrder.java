4c9f93876266836e885205a131582fead5e072d2
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-04-08 10:21:19 +0800

    solutions for 429 levelOrder

--- 429 levelOrder.java
+++ 429 levelOrder.java
@@ -1,4 +1,24 @@
-class Solution {
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+
+class Solution1 {
     public List<List<Integer>> levelOrder(Node root) {
         List<List<Integer>> list = new ArrayList<>();
         leveldfs(root, list, 0);
@@ -22,3 +42,37 @@ class Solution {
     }
 }
 
+class Solution2 {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        if(root == null)
+            return ans;
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        List<Integer> first = new ArrayList<>();
+        first.add(root.val);
+        ans.add(first);
+
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            for(int i = 0; i < size; i++) {
+                Node cur = queue.poll();
+
+                for(Node n: cur.children) {
+                    level.add(n.val);
+                    queue.add(n);
+                }
+            }
+
+            if(level.size() > 0)
+                ans.add(level);
+        }
+
+        return ans;
+    }
+}
