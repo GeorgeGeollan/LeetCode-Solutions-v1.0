9302732619fdaf05a7e14d7d260adf93a6947fa9
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-04-10 11:02:31 +0800

    solutions for 804 uniqueMorseRepresentations

---
+++ 804 uniqueMorseRepresentations.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        HashSet<String> set = new HashSet<>();
+
+        String[] code = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+        for(String word: words) {
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < word.length(); i++) {
+                
+                sb.append(code[word.charAt(i) - 'a']);
+            }
+            
+            set.add(sb.toString());
+        }
+
+        return set.size();
+    }
+}
