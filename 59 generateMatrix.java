9990283ae91e60a8caf078ef490086f45bac1ebd
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-04-13 16:34:41 +0800

    solutions for 59 generateMatrix

---
+++ 59 generateMatrix.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int l = 0, r = n - 1, t = 0, b = n -1;
+        int tar = n * n, num = 1;
+        int[][] ans = new int[n][n];
+        
+        while(num <= tar) {
+            for(int i = l; i <= r; i++)
+                ans[t][i] = num++;
+            
+            t++;
+            
+            for(int i = t; i <= b; i++)
+                ans[i][r] = num++;
+            
+            r--;
+            
+            for(int i = r; i >= l; i--)
+                ans[b][i] = num++;
+            
+            b--;
+            
+            for(int i = b; i >= t; i--)
+                ans[i][l] = num++;
+            
+            l++;
+        }
+        
+        return ans;
+    }
+}
