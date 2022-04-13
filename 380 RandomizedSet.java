12ce371e95e4b5a6b36549a917e824e471337c8c
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-04-13 16:23:16 +0800

    solutions for 380 RandomizedSet

--- 380 RandomizedSet.java
+++ 380 RandomizedSet.java
@@ -1,4 +1,4 @@
-class RandomizedSet {
+class RandomizedSet1 {
     HashMap<Integer, Integer> map;
     List<Integer> list;
     Random rand = new Random();
@@ -33,6 +33,42 @@ class RandomizedSet {
     
     public int getRandom() {
         return list.get(rand.nextInt(list.size()));
+    }
+}
+
+class RandomizedSet2 {
+    static int[] nums = new int[200010];
+    Random random = new Random();
+    Map<Integer, Integer> map = new HashMap<>();
+    int idx = -1;
+
+    public RandomizedSet() {
+
+    }
+    
+    public boolean insert(int val) {
+        if(map.containsKey(val))
+            return false;
+
+        nums[++idx] = val;
+        map.put(val, idx);
+
+        return true;
+    }
+    
+    public boolean remove(int val) {
+        if(!map.containsKey(val))
+            return false;
+
+        int loc = map.remove(val);
+        if(loc != idx)
+            map.put(nums[idx], loc);
+        nums[loc] = nums[idx--];
+        return true;
+    }
+    
+    public int getRandom() {
+        return nums[random.nextInt(idx + 1)];
     }
 }
 
