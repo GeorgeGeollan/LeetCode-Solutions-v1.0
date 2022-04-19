c1b266879f4f85f46d5c46353fb4bafe77adb8ea
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-04-19 09:03:17 +0800

    solutions for 821 shortestToChar

--- 821 shortestToChar.java
+++ 821 shortestToChar.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int[] shortestToChar(String s, char c) {
         int n = s.length();
 
@@ -24,5 +24,29 @@ class Solution {
         }
 
         return pos;
+    }
+}
+
+class Solution2 {
+    public int[] shortestToChar(String s, char c) {
+        int n = s.length();
+
+        int[] ans = new int[n];
+
+        for(int i = 0, idx = -n; i < n; i++) {
+            if(s.charAt(i) == c)
+                idx = i;
+
+            ans[i] = i - idx;
+        }
+
+        for(int i = n - 1, idx = 2 * n; i >= 0; i--) {
+            if(s.charAt(i) == c)
+                idx = i;
+
+            ans[i] = Math.min(ans[i], idx - i);
+        }
+
+        return ans;
     }
 }
