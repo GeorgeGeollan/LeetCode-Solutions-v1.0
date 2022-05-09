89f7a0708fc420c5ad441acd6fa858f4b4edf56f
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-05-09 09:25:06 +0800

    solutions for 17 letterCombinations

--- 17 letterCombinations.java
+++ 17 letterCombinations.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     HashMap<String, String[]> map = new HashMap<>() {{
         put("2", new String[] {"a", "b", "c"});
         put("3", new String[] {"d", "e", "f"});
@@ -36,6 +36,49 @@ class Solution {
         for(String l: ls) {
             sb.append(l);
             backtrack(ds, i + 1, n, sb, ans);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
+
+class Solution2 {
+    List<String> ans;
+    HashMap<Character, String> map;
+    public List<String> letterCombinations(String digits) {
+        ans = new ArrayList<>();
+        
+        map = new HashMap<>();
+        
+        if(digits.length() == 0)
+            return ans;
+        
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+        
+        StringBuilder sb = new StringBuilder();
+        dfs(digits, 0, sb);
+        
+        return ans;
+    }
+    
+    public void dfs(String digits, int idx, StringBuilder sb) {
+        if(idx == digits.length()) {
+            ans.add(sb.toString());
+            return;
+        }
+            
+        char cur = digits.charAt(idx);
+        String com = map.get(cur);
+        
+        for(int i = 0; i < com.length(); i++) {
+            sb.append(com.charAt(i));
+            dfs(digits, idx + 1, sb);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
