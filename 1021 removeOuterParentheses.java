4337ae5c4db86b35a6080fbb95c3fc710befa9c6
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-05-28 14:15:18 +0800

    solutions for 1021 removeOuterParentheses

---
+++ 1021 removeOuterParentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        if(s.length() == 0 || s == null)
+            return "";
+
+        int start = 0;
+        int end = 0;
+        String ans = "";
+
+        Stack<Character> stack = new Stack<>();
+        char[] ch = s.toCharArray();
+
+        for(int i = 0; i < ch.length; i++) {
+            if(ch[i] == '(')
+                stack.push(ch[i]);
+
+            else if(ch[i] == ')')
+                stack.pop();
+
+            end++;
+            
+
+            if(stack.size() == 0) {
+                ans += s.substring(start + 1, end - 1);
+                start = end;
+            }
+        }
+
+        return ans;
+    }
+}
