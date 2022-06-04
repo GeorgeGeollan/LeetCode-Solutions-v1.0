ffd30b48864f3b8a593989cfca530c53bd4de0d0
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-06-04 14:21:51 +0800

    solutions for 929 numUniqueEmails

--- 929 numUniqueEmails.java
+++ 929 numUniqueEmails.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int numUniqueEmails(String[] emails) {
         Set<String> set = new HashSet();
 
@@ -13,6 +13,40 @@ class Solution {
 
             local = local.replaceAll("\\.", "");
             set.add(local + rest);
+        }
+
+        return set.size();
+    }
+}
+
+class Solution2 {
+    public int numUniqueEmails(String[] emails) {
+        HashSet<String> set = new HashSet<>();
+
+        for(String email: emails) {
+            String ans = "";
+            int i = 0;
+            for(i = 0; i < email.length(); i++) {
+                if(email.charAt(i) == '.')
+                    continue;
+
+
+                if(email.charAt(i) == '+') {
+                    while(email.charAt(i) != '@')
+                        i++;
+                }
+
+                if(email.charAt(i) == '@')
+                    break;
+                    
+                ans += email.charAt(i);
+            }
+
+            System.out.println(email.substring(i, email.length()));
+
+            ans += email.substring(i, email.length());
+
+            set.add(ans);
         }
 
         return set.size();
