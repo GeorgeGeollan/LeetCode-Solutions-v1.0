6df82d64064c15b1b2a47005ea472ec47605f3b5
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-06-22 17:08:01 +0800

    solutions for 215 findKthLargest

--- 215 findKthLargest.java
+++ 215 findKthLargest.java
@@ -21,3 +21,21 @@ class Solution2 {
         return nums[nums.length - k];
     }
 }
+
+class Solution3 {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i = 0; i < k; i++) {
+            pq.add(nums[i]);
+        }
+        for(int i = k; i < nums.length; i++) {
+            if(pq.peek() < nums[i]) {
+                pq.poll();
+                pq.add(nums[i]);
+            }
+        }
+
+        return pq.peek();
+    }
+}
