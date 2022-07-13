a3a96b44947931e8ef8a0eae96a03c775fea04d5
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-07-13 11:35:49 +0800

    solutions for 102 levelOrder

---
+++ 102 levelOrder.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        List<List<Integer>> ans = new ArrayList<>();
+
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+
+            for(int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                list.add(cur.val);
+
+                if(cur.left != null)
+                    queue.add(cur.left);
+
+                if(cur.right != null)
+                    queue.add(cur.right);
+            }
+
+            ans.add(list);
+        }
+
+        return ans;
+    }
+}
