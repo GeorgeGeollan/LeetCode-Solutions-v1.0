6dcb6b315c8ad1e2ac148d4b55808833c4eb512f
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-07-17 18:16:22 +0800

    solutions for 565 arrayNesting

--- 565 arrayNesting.java
+++ 565 arrayNesting.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int arrayNesting(int[] nums) {
         int count = 0, res = 0;
         boolean[] visited = new boolean[nums.length];
@@ -18,5 +18,27 @@ class Solution {
         }
 
         return res;
+    }
+}
+
+class Solution2 {
+    public int arrayNesting(int[] nums) {
+        int n = nums.length, ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            int cur = i;
+            int cnt = 0;
+
+            while(nums[cur] != -1) {
+                cnt++;
+                int c = cur;
+                cur = nums[cur];
+                nums[c] = -1;
+            }
+
+            ans = Math.max(cnt , ans);
+        }
+
+        return ans;
     }
 }
