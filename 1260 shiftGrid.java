b6316311d09f323972688e5fc14a372db01c8783
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-07-20 09:58:55 +0800

    solutions for 1260 shiftGrid

--- 1260 shiftGrid.java
+++ 1260 shiftGrid.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         int m = grid.length, n = grid[0].length;
         
@@ -30,6 +30,31 @@ class Solution {
                 cur.add(r);
         }
         
+        return ans;
+    }
+}
+
+class Solution2 {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int m = grid.length, n = grid[0].length;
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int i = 0; i < m; i++) {
+            List<Integer> row = new ArrayList<>();
+            for(int j = 0; j < n; j++) {
+                row.add(0);
+            }
+
+            ans.add(row);
+        }
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                int idx1 = (i * n + k + j) % (m * n);
+                ans.get(idx1  / n).set(idx1 % n, grid[i][j]);
+            }
+        }
+
         return ans;
     }
 }
