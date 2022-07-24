97aec9b8b39aba16d66622d702ba984212fc07ac
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-07-24 13:16:07 +0800

    solutions for 1184 distanceBetweenBusStops

---
+++ 1184 distanceBetweenBusStops.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int distanceBetweenBusStops(int[] distance, int start, int destination) {
+        int ans = Integer.MAX_VALUE;
+        int sum = 0;
+        int n = distance.length;
+        int temp1 = start;
+        int temp2 = destination;
+        while(temp1 != destination) {
+            sum += distance[temp1];
+            temp1 = (temp1 + 1) % n;
+        }
+
+        ans = Math.min(sum, ans);
+        sum = 0;
+
+        while(temp2 != start) {
+            sum += distance[temp2];
+            temp2 = (temp2 + 1) % n;
+        }
+
+        ans = Math.min(sum, ans);
+
+        return ans;
+    }
+}
