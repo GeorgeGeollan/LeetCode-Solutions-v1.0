ef8eafdbdc23cfdd4f731bad39bd4336b40c1eca
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-07-29 09:39:17 +0800

    solutions for 890 findAndReplacePattern

--- 890 findAndReplacePattern.java
+++ 890 findAndReplacePattern.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
         HashMap<Character, Character> map = new HashMap<>();
         HashMap<Character, Character> map2 = new HashMap<>();
@@ -32,5 +32,35 @@ class Solution {
         }
 
         return list;
+    }
+}
+
+class Solution2 {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> ans = new ArrayList<>();
+        int[] map = new int[26], vis = new int[26];
+        
+        for(String word: words) {
+            Arrays.fill(map, -1);
+            Arrays.fill(vis, 0);
+            boolean ok = true;
+
+            for(int i = 0; i < pattern.length() && ok; i++) {
+                int c1 = word.charAt(i) - 'a', c2 = pattern.charAt(i) - 'a';
+
+                if(map[c1] == -1 && vis[c2] == 0) {
+                    map[c1] = c2;
+                    vis[c2] = 1;
+                }
+
+                else if(map[c1] != c2)
+                    ok = false;
+            }
+
+            if(ok)
+                ans.add(word);
+        }
+
+        return ans;
     }
 }
