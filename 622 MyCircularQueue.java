023a15ba6bce31041a451a6d914687ebd6e357ad
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-08-02 09:41:03 +0800

    solutions for 622 MyCircularQueue

---
+++ 622 MyCircularQueue.java
@@ -0,0 +1,51 @@
+class MyCircularQueue {
+    int k, head, tail;
+    int[] nums;
+
+    public MyCircularQueue(int k) {
+        this.k = k;
+        nums = new int[k];
+    }
+    
+    public boolean enQueue(int value) {
+        if(isFull())
+            return false;
+
+        nums[tail % k] = value;
+        return ++tail >= 0;
+    }
+    
+    public boolean deQueue() {
+        if(isEmpty())
+            return false;
+
+        return ++head >= 0;
+    }
+    
+    public int Front() {
+        return isEmpty() ? -1 : nums[head % k];
+    }
+    
+    public int Rear() {
+        return isEmpty() ? -1 : nums[(tail - 1) % k];
+    }
+    
+    public boolean isEmpty() {
+        return head == tail;
+    }
+    
+    public boolean isFull() {
+        return tail - head == k;
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
