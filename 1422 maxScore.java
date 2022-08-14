e6b4962b22536ebecc56aab766abc7747aa89d88
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-08-14 12:48:34 +0800

    solutions for 1422 maxScore

---
+++ 1422 maxScore.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxScore(String s) {
+        int total = s.length();
+        int cnt_0 = 0;
+        int max = 0;
+        int dp[] = new int[total];
+
+        for(int i = 0; i < total; i++) {
+            if(s.charAt(i) == '0')
+                cnt_0++;
+
+            dp[i] = cnt_0;
+        }
+
+        for(int i = 0; i < total - 1; i++) {
+            int a = dp[i];
+            int b = dp[total - 1] - a;
+
+            int len = total - i - 1;
+            System.out.println(a + len - b);
+
+            max = Math.max(max, a + len - b);
+        }
+
+        return max;
+    }
+}
