6c9904b0795c937d50bd122019500f6fb6be1a01
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-08-21 14:38:24 +0800

    solutions for 1455 isPrefixOfWord

---
+++ 1455 isPrefixOfWord.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] words = sentence.split(" ");
+        int n = searchWord.length();
+
+        for(int i = 0; i < words.length; i++) {
+            int m = words[i].length();
+            
+            if(m < n)
+                continue;
+            
+            if(words[i].substring(0, n).equals(searchWord)) {
+                return i + 1;
+            }
+        }
+
+        return -1;
+    }
+}
