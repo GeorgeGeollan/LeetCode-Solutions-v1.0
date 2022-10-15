7d2cb7b811729e75387e64e6eadaa238a08ec11e
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-10-15 22:22:55 +0800

    solutions for 1441 buildArray

---
+++ 1441 buildArray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> ans = new ArrayList<>();
+
+        int m = target.length;
+
+        for(int i = 1, j = 0; i <= n && j < m; i++) {
+            ans.add("Push");
+
+            if(target[j] != i)
+                ans.add("Pop");
+
+            else
+                j++;
+        }
+
+        return ans;
+    }
+}
