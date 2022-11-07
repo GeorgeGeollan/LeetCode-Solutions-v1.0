d89fe37e6268a38ae669fba6501d85059d42e27c
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-11-07 08:53:08 +0800

    solutions for 1323 maximum69Number

---
+++ 1323 maximum69Number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maximum69Number (int num) {
+        StringBuilder sb = new StringBuilder();
+
+        String str = String.valueOf(num);
+        char[] nums = str.toCharArray();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == '6') {
+                nums[i] = '9';
+                break;
+            }
+                
+        }
+
+        return Integer.valueOf(new String(nums));
+    }
+}
