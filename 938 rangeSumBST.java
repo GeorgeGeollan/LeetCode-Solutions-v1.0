85a1b3aecf2305e4db06560d5ee881dd78873672
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-12-07 17:07:29 +0800

    solutions for 938 rangeSumBST

--- 938 rangeSumBST.java
+++ 938 rangeSumBST.java
@@ -24,11 +24,15 @@ class Solution {
     }
     
     public void dfs(TreeNode root, int low, int high) {
-        if(root == null)
+        if(root == null) {
             return;
+        }
             
-        if(root.val >= low && root.val <= high)
+            
+        if(root.val >= low && root.val <= high) {
             sum += root.val;
+        }
+            
         
         dfs(root.left, low, high);
         dfs(root.right, low, high);
