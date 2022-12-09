4868a400900b97ab4ea349913bd90780d859a2cc
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-12-09 10:00:13 +0800

    solutions for 1026 maxAncestorDiff

--- 1026 maxAncestorDiff.java
+++ 1026 maxAncestorDiff.java
@@ -13,7 +13,7 @@
  *     }
  * }
  */
-class Solution {
+class Solution1 {
     int ans = 0;
     
     public int maxAncestorDiff(TreeNode root) {
@@ -31,5 +31,34 @@ class Solution {
         dfs(root.left, Math.max(preMax, root.val), Math.min(preMin, root.val));
         dfs(root.right, Math.max(preMax, root.val), Math.min(preMin, root.val));
         
+    }
+}
+
+class Solution2 {
+    int res = Integer.MIN_VALUE;
+    public int maxAncestorDiff(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+
+        helper(root, root.val, root.val);
+
+        return res;
+    }
+
+    public void helper(TreeNode root, int min, int max) {
+        if(root == null) {
+            return;
+        }
+
+        max = Math.max(max, root.val);
+        min = Math.min(min, root.val);
+
+        if(root.left == null && root.right == null) {
+            res = Math.max(res, Math.abs(max - min));
+        }
+
+        helper(root.left, min, max);
+        helper(root.right, min, max);
     }
 }
