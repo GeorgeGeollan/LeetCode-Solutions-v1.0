1e61654356804354071aa76139f99ee587ab73be
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-12-14 08:45:51 +0800

    solutions for 198 rob

--- 198 rob.java
+++ 198 rob.java
@@ -1,27 +1,18 @@
 class Solution1 {
     public int rob(int[] nums) {
-        int max = 0, n = nums.length;
-        int dp[] = new int[n + 1];
-        
-        if(n == 1)
-            return nums[0];
+        int n = nums.length;
+        if(n == 0)
+            return 0;
 
-        if(n == 2)
-            return Math.max(nums[0], nums[1]);
-
+        int[] dp = new int[n + 1];
         dp[0] = 0;
         dp[1] = nums[0];
-        dp[2] = nums[1];
-        
-        max = Math.max(dp[1], dp[2]);
 
-        for(int i = 3; i <= n; i++) {
-            dp[i] = nums[i - 1] + Math.max(dp[i - 2], dp[i - 3]);
-            max = Math.max(max, dp[i]);
+        for(int i = 2; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i - 1] + dp[i - 2]);
         }
 
-
-        return max;
+        return dp[n];
     }
 }
 
