dac489c78c5de6ac1e67178f0f7d03a2c415fdf9
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2022-12-18 11:08:31 +0800

    solutions for 739 dailyTemperatures

--- 739 dailyTemperatures.java
+++ 739 dailyTemperatures.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
         int[] res = new int[n];
@@ -28,5 +28,27 @@ class Solution {
             }
         }
         return res;
+    }
+}
+
+
+class Solution2 {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int length = temperatures.length;
+        int[] ans = new int[length];
+        Deque<Integer> stack = new LinkedList<>();
+
+        for(int i = 0; i < length; i++) {
+            int temperature = temperatures[i];
+
+            while(!stack.isEmpty() && temperature > temperatures[stack.peek()]) {
+                int prevIdx = stack.pop();
+                ans[prevIdx] = i - prevIdx;
+            }
+
+            stack.push(i);
+        }
+
+        return ans;
     }
 }
