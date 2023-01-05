156df306ca7d386278096edbfc8ec9d305525861
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-01-05 09:55:14 +0800

    solutions for 452 findMinArrowShots

--- 452 findMinArrowShots.java
+++ 452 findMinArrowShots.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int findMinArrowShots(int[][] points) {
         Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
         int arrow = 1;
@@ -13,5 +13,41 @@ class Solution {
         }
         
         return arrow;
+    }
+}
+
+class Solution2 {
+    public int findMinArrowShots(int[][] points) {
+        if(points.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(points, new Comparator<int[]>() {
+            public int compare(int[] p1, int[] p2) {
+                if(p1[1] > p2[1]) {
+                    return 1;
+                }
+                
+                else if(p1[1] < p2[1]) {
+                    return -1;
+                }
+
+                else {
+                    return 0;
+                }
+            }
+        });
+
+        int pos = points[0][1];
+        int ans = 1;
+
+        for(int[] point: points) {
+            if(point[0] > pos) {
+                pos = point[1];
+                ++ans;
+            }
+        }
+
+        return ans;
     }
 }
