ce3bd6fd801e8e34a7004132f3efe4f0007ebcc2
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-01-06 08:55:21 +0800

    solutions for 2180 countEven

---
+++ 2180 countEven.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countEven(int num) {
+        int cnt = 0;
+
+        for(int i = 1; i <= num; i++) {
+            int sum = 0;
+            int temp = i;
+
+            while(temp > 0) {
+                sum += temp % 10;
+                temp = temp / 10;
+            }
+
+            if(sum % 2 == 0) {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+}
