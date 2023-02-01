773b9c091a4820a930042aeff3016662e0a87e2e
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-02-01 08:51:12 +0800

    solutions for 2325 decodeMessage

---
+++ 2325 decodeMessage.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String decodeMessage(String key, String message) {
+        Map<Character, Character> map = new HashMap<>();
+        char cur = 'a';
+
+        for(int i = 0; i < key.length(); i++) {
+            if(key.charAt(i) == ' ') {
+                continue;
+            }
+
+            if(map.containsKey(key.charAt(i))) {
+                continue;
+            }
+
+            map.put(key.charAt(i), cur);
+            System.out.println(cur);
+            cur = (char)(cur + 1);
+        }
+
+        String ans = "";
+
+        for(int i = 0; i < message.length(); i++) {
+            if(message.charAt(i) == ' ') {
+                ans += " ";
+            }
+
+            else {
+                ans += map.get(message.charAt(i));
+            }
+        }
+
+        return ans;
+    }
+}
