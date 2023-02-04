1c40e006032de2c0758cdc24226e6267a9336560
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-02-04 15:11:27 +0800

    solutions for 1798 getMaximumConsecutive

---
+++ 1798 getMaximumConsecutive.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int getMaximumConsecutive(int[] coins) {
+        int res = 1;
+        Arrays.sort(coins);
+
+        for(int coin: coins) {
+            if(coin > res) {
+                break;
+            }
+
+            res += coin;
+        }
+
+        return res;
+    }
+}
