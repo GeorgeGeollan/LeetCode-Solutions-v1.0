f0cafcc94d96d1a3cd9cf666f377cdcb36292866
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-03-05 12:48:59 +0800

    solutions for 2104 subArrayRanges

--- 2104 subArrayRanges.java
+++ 2104 subArrayRanges.java
@@ -9,7 +9,6 @@ class Solution {
             for(int j = i + 1; j < nums.length; j++) {
                 max = Math.max(max, nums[j]);
                 min = Math.min(min, nums[j]);
-
                 sum += max - min;
             }
         }
