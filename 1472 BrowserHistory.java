980cc458e29ea215a532d62b8dd3f9a886f31116
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-03-18 17:50:20 +0800

    solutions for 1472 BrowserHistory

---
+++ 1472 BrowserHistory.java
@@ -0,0 +1,33 @@
+class BrowserHistory {
+    String[] history;
+    int cur, range;
+
+    public BrowserHistory(String homepage) {
+        history = new String[5000];
+        cur = -1;
+        this.visit(homepage);
+    }
+    
+    public void visit(String url) {
+        history[++cur] = url;
+        range = cur;
+    }
+    
+    public String back(int steps) {
+        cur = Math.max(0, cur - steps);
+        return history[cur];
+    }
+    
+    public String forward(int steps) {
+        cur = Math.min(cur + steps, range);
+        return history[cur];
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
