b762e28e32e413770a017ddbee84cc8e66b8bbb2
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-04-01 22:49:35 +0800

    solutions for 704 search

--- 704 search.java
+++ 704 search.java
@@ -1,20 +1,23 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
+        int l = 0 , r = nums.length - 1;
 
-        while(left < right) {
-            int mid = (right - left) / 2 + left;
-            if(nums[mid] == target)
+        while(l <= r) {
+            int mid = (l + r) / 2;
+
+            if(nums[mid] == target) {
                 return mid;
+            }
 
-            else if(nums[mid] > target)
-                right = mid - 1;
+            else if(nums[mid] > target) {
+                r = mid - 1;
+            }
 
-            else
-                left = mid + 1;
+            else {
+                l = mid + 1;
+            }
         }
 
-        return nums[left] == target ? left : -1;
+        return -1;
     }
 }
