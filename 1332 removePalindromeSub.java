7fe446256f988bdf8f6fb6c4f2a025535958d27f
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-04-02 11:23:24 +0800

    solutions for 1332 removePalindromeSub

--- 1332 removePalindromeSub.java
+++ 1332 removePalindromeSub.java
@@ -3,8 +3,10 @@ class Solution {
         int i = 0, j = s.length() - 1;
 
         while(i < j) {
-            if(s.charAt(i) != s.charAt(j))
+            if(s.charAt(i) != s.charAt(j)) {
                 return 2;
+            }
+                
             i++;
             j--;
         }
