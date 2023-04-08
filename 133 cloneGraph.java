fc1103531a9a50abb008c8bbde0e15dbec6fe1ac
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-04-08 20:13:19 +0800

    solutions for 133 cloneGraph

--- 133 cloneGraph.java
+++ 133 cloneGraph.java
@@ -18,7 +18,7 @@ class Node {
 }
 */
 
-class Solution {
+class Solution1 {
     public Node cloneGraph(Node node) {
         Map<Node, Node> map = new HashMap<>();
         
@@ -40,6 +40,29 @@ class Solution {
         }
             
         
+        return clone;
+    }
+}
+
+class Solution2 {
+    private Map<Node, Node> visited = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if(node == null) {
+            return null;
+        }
+
+        if(visited.containsKey(node)) {
+            return visited.get(node);
+        }
+
+        Node clone = new Node(node.val, new ArrayList<>());
+        visited.put(node, clone);
+
+        for(Node neighbor: node.neighbors) {
+            clone.neighbors.add(cloneGraph(neighbor));
+        }
+
         return clone;
     }
 }
