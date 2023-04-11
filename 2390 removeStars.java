06e309e778de9407e47ef68317bc70f91ee49e8d
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-04-11 10:51:31 +0800

    solutions for 2390 removeStars

---
+++ 2390 removeStars.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String removeStars(String s) {
+        Stack<Character> stk = new Stack<>();
+        
+        for(char ch: s.toCharArray()) {
+            if(ch == '*') {
+                stk.pop();
+            }
+
+            else {
+                stk.push(ch);
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+
+        while(!stk.isEmpty()) {
+            sb.append(stk.pop());
+        }
+        
+        return sb.reverse().toString();
+    }
+}
