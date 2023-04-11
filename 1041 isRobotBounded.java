f7701d5bfd68c8010c0a8131feb1243b65fc1fd7
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-04-11 14:37:57 +0800

    solutions for 1041 isRobotBounded

--- 1041 isRobotBounded.java
+++ 1041 isRobotBounded.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public boolean isRobotBounded(String instructions) {
         int[][] dirs = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
         int x = 0, y = 0, i = 0;
@@ -17,5 +17,49 @@ class Solution {
         }
 
         return x == 0 && y == 0 || i != 0;    
+    }
+}
+
+class Solution2 {
+    public boolean isRobotBounded(String instructions) {
+        int x = 0, y = 0;
+        int ptr = 0;
+
+        instructions = instructions + instructions + instructions + instructions;
+        for(char ch: instructions.toCharArray()) {
+            if(ch == 'L') {
+                ptr--;
+                continue;
+            }
+
+            if(ch == 'R') {
+                ptr++;
+                continue;
+            }
+
+            while(ptr < 0) {
+                ptr += 4;
+            }
+
+            if(ptr % 4 == 0) {
+                y++;
+            }
+
+            else if(ptr % 4 == 1) {
+                x--;
+            }
+
+            else if(ptr % 4 == 2) {
+                y--;
+            }
+
+            else if(ptr % 4 == 3) {
+                x++;
+            }
+
+            System.out.println(x + " " + y);
+        }
+
+        return x == 0 && y == 0;
     }
 }
