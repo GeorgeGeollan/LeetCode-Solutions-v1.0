5f04ada976ac8f5bff062644e07996245d41fcaf
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-04-18 20:12:35 +0800

    solutions for 1472 BrowserHistory

--- 1472 BrowserHistory.java
+++ 1472 BrowserHistory.java
@@ -23,11 +23,3 @@ class BrowserHistory {
         return history[cur];
     }
 }
-
-/**
- * Your BrowserHistory object will be instantiated and called as such:
- * BrowserHistory obj = new BrowserHistory(homepage);
- * obj.visit(url);
- * String param_2 = obj.back(steps);
- * String param_3 = obj.forward(steps);
- */
