740b5ef71b39d8ee9307918bd19a0793b5f5eed7
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-04-19 09:13:03 +0800

    solutions for Offer 05 replaceSpace

--- Offer 05 replaceSpace.java
+++ Offer 05 replaceSpace.java
@@ -1,16 +1,17 @@
 class Solution {
     public String replaceSpace(String s) {
-       StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
-       for(int i = 0; i < s.length(); i++)
-       {
-           if(s.charAt(i) == ' ')
-               sb.append("%20");
-            
-            else
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == ' ') {
+                sb.append("%20");
+            }
+
+            else {
                 sb.append(s.charAt(i));
-       }
+            }
+        }
 
-       return sb.toString();
+        return sb.toString();
     }
 }
