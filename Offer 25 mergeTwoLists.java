1e394362ba62afea59442c409e004b9e1a5d2072
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-04-25 08:59:45 +0800

    solutions for Offer 25 mergeTwoLists

--- Offer 25 mergeTwoLists.java
+++ Offer 25 mergeTwoLists.java
@@ -10,25 +10,22 @@ class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode();
         ListNode ptr = dummy;
-        while(l1 != null && l2 != null)
-        {
-            if(l1.val <= l2.val)
-            {
-                dummy.next = l1;
-                dummy = dummy.next;
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                ptr.next = l1;
                 l1 = l1.next;
             }
 
-            else
-            {
-                dummy.next = l2;
-                dummy = dummy.next;
+            else {
+                ptr.next = l2;
                 l2 = l2.next;
             }
+
+            ptr = ptr.next;
         }
 
-        dummy.next = l1 == null ? l2 : l1;
-        
-        return ptr.next;
+        ptr.next = l1 == null ? l2 : l1;
+
+        return dummy.next;
     }
 }
