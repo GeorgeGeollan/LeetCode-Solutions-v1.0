435a37f4a6cf00d652ec9892896c7ef069be0143
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-04-28 15:06:17 +0800

    solutions for Offer 57 twoSum

--- Offer 57 twoSum.java
+++ Offer 57 twoSum.java
@@ -1,22 +1,23 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
+        int left = 0, right = nums.length - 1;
 
-        while(left < right)
-        {
+        while(left < right) {
             int sum = nums[left] + nums[right];
 
-            if(sum == target)
-                return new int[]{nums[left], nums[right]};
+            if(sum == target) {
+                return new int[] {nums[left], nums[right]};
+            }
 
-            else if(sum < target)
-                left++;
-            
-            else
+            else if(sum > target) {
                 right--;
+            }
+
+            else if(sum < target) {
+                left++;
+            }
         }
 
-        return new int[0];
+        return null;
     }
 }
