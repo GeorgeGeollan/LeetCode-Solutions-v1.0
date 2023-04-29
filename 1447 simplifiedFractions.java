95e2fc1132152011a586e2028d86e2fc2e2c4d66
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-04-29 22:15:23 +0800

    solutions for 1447 simplifiedFractions

--- 1447 simplifiedFractions.java
+++ 1447 simplifiedFractions.java
@@ -34,6 +34,7 @@ class Solution2 {
     
     public List<String> simplifiedFractions(int n) {
         List<String> ans = new ArrayList<>();
+        
         for(int i = 1; i < n; i++) {
             for(int j = i + 1; j <= n; j++) {
                 if(gcd(i, j) == 1)
