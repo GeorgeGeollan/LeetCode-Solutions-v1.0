6fc619d2975a1b324b977b0b4d3c9e5eede98fc2
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-05-07 23:34:18 +0800

    solutions for Offer 03 findRepeatNumber

--- Offer 03 findRepeatNumber.java
+++ Offer 03 findRepeatNumber.java
@@ -1,50 +1,19 @@
-class Solution1 {
-    public int findRepeatNumber(int[] nums) {
-        Set repeated = new HashSet();
-
-        for(int num: nums)
-        {
-            if(!repeated.add(num))
-            {
-                return num;
-            }
-        }
-
-        return -1;
-    }
-}
-
-class Solution2 {
-    public int findRepeatNumber(int[] nums) {
-        Arrays.sort(nums);
-
-        for(int i = 0; i < nums.length - 1; i++)
-        {
-            if(nums[i] == nums[i + 1])
-                return nums[i];
-        }
-
-        return -1;
-    }
-}
-
-class Solution3 {
+class Solution {
     public int findRepeatNumber(int[] nums) {
         int i = 0;
 
-        while(i < nums.length)
-        {
-            if(nums[i] == i)
-            {
+        while(i < nums.length) {
+            if(nums[i] == i) {
                 i++;
                 continue;
             }
 
-            if(nums[nums[i]] == nums[i])
+            if(nums[i] == nums[nums[i]]) {
                 return nums[i];
+            }
 
             int temp = nums[i];
-            nums[i] = nums[temp];
+            nums[i] = nums[nums[i]];
             nums[temp] = temp;
         }
 
