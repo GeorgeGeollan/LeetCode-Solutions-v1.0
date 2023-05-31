b587d8a47a96450bafa0324c3855c0c837dd1b90
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-05-31 20:37:58 +0800

    solutions for Offer 66 constructArr.java

--- Offer 66 constructArr.java
+++ Offer 66 constructArr.java
@@ -1,18 +1,24 @@
 class Solution {
     public int[] constructArr(int[] a) {
-        int[] ans = new int[a.length];
-        Arrays.fill(ans, 1);
+        int n = a.length;
+        int[] ans = new int[n];
+        
+        if(n == 0) {
+            return new int[0];
+        }
+        
+        ans[0] = 1;
 
-        for(int i = 1; i < a.length; i++) {
+        for(int i = 1; i < n; i++) {
             ans[i] = a[i - 1] * ans[i - 1];
         }
 
         int product = 1;
-        for(int i = a.length - 2; i >= 0; i--) {
+        for(int i = n - 2; i >= 0; i--) {
             product *= a[i + 1];
             ans[i] = ans[i] * product;
         }
-        
+
         return ans;
     }
 }
