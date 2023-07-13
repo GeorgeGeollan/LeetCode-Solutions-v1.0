2d0dbbedb69bb97771e66d8a85cb69d6ddabd033
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-07-13 15:10:49 +0800

    solutions for Offer 20 isNumber.java

--- Offer 20 isNumber.java
+++ Offer 20 isNumber.java
@@ -1,32 +1,44 @@
 class Solution {
     public boolean isNumber(String s) {
-        Map[] states = 
-        {
-            new HashMap<>() {{ put(' ', 0); put('s', 1); put('d', 2); put('.', 4); }}, // 0.
-            new HashMap<>() {{ put('d', 2); put('.', 4); }},                           // 1.
-            new HashMap<>() {{ put('d', 2); put('.', 3); put('e', 5); put(' ', 8); }}, // 2.
-            new HashMap<>() {{ put('d', 3); put('e', 5); put(' ', 8); }},              // 3.
-            new HashMap<>() {{ put('d', 3); }},                                        // 4.
-            new HashMap<>() {{ put('s', 6); put('d', 7); }},                           // 5.
-            new HashMap<>() {{ put('d', 7); }},                                        // 6.
-            new HashMap<>() {{ put('d', 7); put(' ', 8); }},                           // 7.
-            new HashMap<>() {{ put(' ', 8); }}                                         // 8.
-        };
-
-        int p = 0;
-        char t;
-        for(char c : s.toCharArray()) 
-        {
-            if(c >= '0' && c <= '9') t = 'd';
-            else if(c == '+' || c == '-') t = 's';
-            else if(c == 'e' || c == 'E') t = 'e';
-            else if(c == '.' || c == ' ') t = c;
-            else t = '?';
-
-            if(!states[p].containsKey(t)) return false;
-            p = (int)states[p].get(t);
+        if(s == null || s.length() == 0) {
+            return false;
         }
 
-        return p == 2 || p == 3 || p == 7 || p == 8;
+        boolean isNumber = false, isE = false, isDot = false;
+        char[] ch = s.trim().toCharArray();
+        for(int i = 0; i < ch.length; i++) {
+            if(ch[i] >= '0' && ch[i] <= '9') {
+                isNumber = true;
+            }
+
+            else if(ch[i] == '.') {
+                if(isDot || isE) {
+                    return false;
+                }
+
+                isDot = true;
+            }
+
+            else if(ch[i] == 'E' || ch[i] == 'e') {
+                if(!isNumber || isE) {
+                    return false;
+                }
+                
+                isE = true;
+                isNumber = false;
+            }
+
+            else if(ch[i] == '+' || ch[i] == '-') {
+                if(i != 0 && ch[i - 1] != 'e' && ch[i - 1] != 'E') {
+                    return false;
+                }
+            }
+
+            else {
+                return false;
+            }
+        }
+
+        return isNumber;
     }
 }
