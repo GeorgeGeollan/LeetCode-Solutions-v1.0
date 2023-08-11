33093a2d5148bdeecee5e3c421ffd8c002d234fc
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-08-11 08:15:43 +0800

    solutions for 1572 diagonalSum.java

--- 1572 diagonalSum.java
+++ 1572 diagonalSum.java
@@ -1,13 +1,12 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
-        int sum = 0;
-        int n = mat.length;
-        for(int i = 0; i < n; i++) {
-            sum += mat[i][i];
+        int sum = 0, n = mat.length;
+        boolean isOdd = n % 2 == 1;
 
-            sum += mat[i][n - i - 1];
+        for(int i = 0; i < n; i++) {
+            sum += (mat[i][i] + mat[n - i - 1][i]);
         }
 
-        return n % 2 == 0 ? sum : sum - mat[n / 2][n / 2];
+        return isOdd ? sum - mat[n / 2][n / 2] : sum;
     }
 }
