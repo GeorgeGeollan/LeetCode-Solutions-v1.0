3f32ca9da34b54f3b273f73f92a6eb5b89f05c7f
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-08-13 19:00:53 +0800

    solutions for 134 canCompleteCircuit.java

--- 134 canCompleteCircuit.java
+++ 134 canCompleteCircuit.java
@@ -1,12 +1,13 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int start = 0;
-
         int n = gas.length, remain = 0;
         int i = 0, sum = 0;
 
-        for(int j = 0; j < n; j++)
+        for(int j = 0; j < n; j++) {
             sum += gas[j] - cost[j];
+        }
+            
 
         if(sum < 0)
             return -1;
