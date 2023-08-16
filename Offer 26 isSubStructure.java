c1f056d4f61103131338ed3aaa487e2e9d60edd3
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-08-16 10:24:23 +0800

    solutions for Offer 26 isSubStructure.java

--- Offer 26 isSubStructure.java
+++ Offer 26 isSubStructure.java
@@ -9,17 +9,19 @@
  */
 class Solution {
     public boolean isSubStructure(TreeNode A, TreeNode B) {
-        return (A != null && B != null) && (recur(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));        
+        return (A != null && B != null) && (recur(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
+        
     }
 
-    public boolean recur(TreeNode A, TreeNode B)
-    {
-        if(B == null)
+    public boolean recur(TreeNode A, TreeNode B) {
+        if(B == null) {
             return true;
+        }
 
-        if(A == null || A.val != B.val)
+        if((A == null) || (A.val != B.val)) {
             return false;
-        
-        return recur(A.left, B.left) && recur(A.right, B.right);
+        }
+
+        return recur(A.right, B.right) && recur(A.left, B.left); 
     }
 }
