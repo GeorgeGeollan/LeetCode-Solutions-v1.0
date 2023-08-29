31e1058702e36d6c3f1618fd95f4ed1842cc7ccf
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-08-29 09:22:58 +0800

    solutions for Offer 14 -I cuttingRope.java

--- Offer 14 -I cuttingRope.java
+++ Offer 14 -I cuttingRope.java
@@ -1,15 +1,21 @@
 class Solution {
     public int cuttingRope(int n) {
-        if(n <= 3)
-            return n - 1;
+       if(n <= 3) {
+           return n - 1;
+       }
 
-        if(n % 3 == 0)
-            return (int)(Math.pow(3, n / 3));
+        int numThrees = n / 3;
+        int remains = n % 3;
 
-        if(n % 3 == 1)
-            return (int)(Math.pow(3, n / 3 - 1) * 2 * 2);
-        
-        else
-            return (int)(Math.pow(3, n / 3)) * 2;
+        if(remains == 0) {
+            return (int)(Math.pow(3, numThrees));
+        }
+
+        if(remains == 1) {
+            return (int)(Math.pow(3, numThrees - 1)) * 4;
+        }
+
+       
+        return (int)(Math.pow(3, numThrees)) * 2;
     }
 }
