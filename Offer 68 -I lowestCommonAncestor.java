4f3d4dab1fec93b3c07779e3edebc87cbef5390b
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-09-01 14:34:28 +0800

    solutions for Offer 68 -I lowestCommonAncestor.java

--- Offer 68 -I lowestCommonAncestor.java
+++ Offer 68 -I lowestCommonAncestor.java
@@ -9,25 +9,14 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        System.out.println(root.val);
-        if(root == null) {
-            return null;
-        }
-
-        if((root.val == p.val) || (root.val == q.val)) {
-            return root;
-        }
-
-        if((root.val > p.val && root.val < q.val) || (root.val < p.val && root.val > q.val)) {
-            return root;
-        }
-
-        else if(root.val > p.val) {
+        if(root.val > p.val && root.val > q.val) {
             return lowestCommonAncestor(root.left, p, q);
         }
 
-        else {
+        if(root.val < q.val && root.val < p.val) {
             return lowestCommonAncestor(root.right, p, q);
         }
+
+        return root;
     }
 }
