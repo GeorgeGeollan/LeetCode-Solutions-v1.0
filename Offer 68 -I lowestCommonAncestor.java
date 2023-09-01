9e1e84722470ea9dceb6b2224eb3ecf49ed2ad5c
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-09-01 11:29:37 +0800

    solutions for Offer 68 -I lowestCommonAncestor.java

--- Offer 68 -I lowestCommonAncestor.java
+++ Offer 68 -I lowestCommonAncestor.java
@@ -7,55 +7,27 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-class Solution1 {
+class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null)
+        System.out.println(root.val);
+        if(root == null) {
             return null;
-        
-        if(root.val > p.val && root.val < q.val)
-            return root;
-        
-        if(root.val < p.val && root.val > q.val)
-            return root;
-
-        if(root.val == p.val || root.val == q.val)
-            return root;
-        
-        if(root.val > p.val && root.val > q.val)
-            return lowestCommonAncestor(root.left, p, q);
-
-        if(root.val < p.val && root.val < q.val)
-            return lowestCommonAncestor(root.right, p, q);
-
-        return null;
-    }
-}
-
-# improve and concise version for Solution1
-class Solution2 {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root.val > p.val && root.val > q.val)
-            return lowestCommonAncestor(root.left, p, q);
-
-        if(root.val < p.val && root.val < q.val)
-            return lowestCommonAncestor(root.right, p, q);
-
-        return root;
-    }
-}
-
-class Solution3 {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        while(root != null)
-        {
-            if(root.val < p.val && root.val < q.val)
-                root = root.right;
-            else if(root.val > p.val && root.val > q.val)
-                root = root.left;
-            else
-                break;
         }
 
-        return root;
+        if((root.val == p.val) || (root.val == q.val)) {
+            return root;
+        }
+
+        if((root.val > p.val && root.val < q.val) || (root.val < p.val && root.val > q.val)) {
+            return root;
+        }
+
+        else if(root.val > p.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+
+        else {
+            return lowestCommonAncestor(root.right, p, q);
+        }
     }
 }
