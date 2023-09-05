44a824bf7d8f83a9c1c84d9c281ac139c1c7ea53
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-09-05 11:15:15 +0800

    solutions for Offer 31 validateStackSequences.java

--- Offer 31 validateStackSequences.java
+++ Offer 31 validateStackSequences.java
@@ -1,54 +1,15 @@
-class Solution1 {
-    public boolean validateStackSequences(int[] pushed, int[] popped) {
-        Stack<Integer> stack = new Stack<>();
-        int count = 0;
-
-        for(int pop: popped)
-        {
-            if(count < pushed.length && !stack.contains(pop))
-            {
-                while(pushed[count] != pop)
-                {
-                    stack.add(pushed[count++]);
-                }
-
-                stack.add(pushed[count++]);
-                stack.pop();
-            }
-        
-                
-            else
-            {
-                // System.out.println(stack);
-                if(stack.isEmpty())
-                    return false;
-
-                int push = stack.pop();
-                
-                if(push != pop)
-                    return false;
-            }
-        }
-
-        return true;
-    }
-}
-
-class Solution2 {
+class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
         Stack<Integer> stack = new Stack<>();
         int i = 0;
 
-        for(int num: pushed)
-        {
+        for(int num: pushed) {
             stack.push(num);
-            
-            while(!stack.isEmpty() && stack.peek() == popped[i])
-            {
+
+            while(!stack.isEmpty() && stack.peek() == popped[i]) {
                 stack.pop();
                 i++;
             }
-                
         }
 
         return stack.isEmpty();
