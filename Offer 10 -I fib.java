7efe1f4012c2975c83973416acd16eaaac5925c1
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-09-07 10:33:38 +0800

    solutions for Offer 10 -I fib.java

--- Offer 10 -I fib.java
+++ Offer 10 -I fib.java
@@ -1,29 +1,17 @@
-class Solution1 {
+class Solution {
     public int fib(int n) {
-        if(n < 2)
+        if(n <= 1) {
             return n;
+        }
+
         int[] dp = new int[n + 1];
         dp[0] = 0;
         dp[1] = 1;
 
-        for(int i = 2; i <= n; i++)
+        for(int i = 2; i <= n; i++) {
             dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
-
-        return dp[n];
-    }
-}
-
-class Solution2 {
-    public int fib(int n) {
-        int a = 0, b = 1, sum = 0;
-
-        for(int i = 0; i < n; i++)
-        {
-            sum = (a + b) % 1000000007;
-            a = b;
-            b = sum;
         }
 
-        return a;
+        return dp[n];
     }
 }
