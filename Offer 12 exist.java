b15740df1959e191a638881653fe431a5a784981
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-09-11 10:52:04 +0800

    solutions for Offer 12 exist.java

--- Offer 12 exist.java
+++ Offer 12 exist.java
@@ -1,33 +1,34 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        char[] words = word.toCharArray();
-
-        for(int i = 0; i < board.length; i++)
-        {
-            for(int j = 0; j < board[0].length; j++)
-            {
-                if(dfs(board, words, i, j, 0))
-                    return true;
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(board[i][j] == word.charAt(0)) {
+                    if(dfs(board, word, i, j, 0)) {
+                        return true;
+                    }
+                }
             }
         }
 
         return false;
     }
 
-    public boolean dfs(char[][] board, char[] word, int i, int j, int k)
-    {
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || word[k] != board[i][j])
+    public boolean dfs(char[][] board, String word, int i, int j, int idx) {
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(idx) || idx >= word.length()) {
             return false;
-
-        if(k == word.length - 1)
-            return true;
+        }
 
         board[i][j] = '\0';
 
-        boolean res = dfs(board, word, i - 1, j, k + 1) ||  dfs(board, word, i + 1, j, k + 1)
-                   || dfs(board, word, i, j - 1, k + 1) ||  dfs(board, word, i, j + 1, k + 1);
+        if(idx == word.length() - 1) {
+            return true;
+
+        }
         
-        board[i][j] = word[k];
-        return res;
+        boolean ans = dfs(board,  word, i + 1, j, idx + 1) || dfs(board,  word, i, j + 1, idx + 1) 
+        || dfs(board, word, i - 1, j, idx + 1) || dfs(board, word, i, j - 1, idx + 1);
+
+        board[i][j] = word.charAt(idx);
+        return ans;
     }
 }
