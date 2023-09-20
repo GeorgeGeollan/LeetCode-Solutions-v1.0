11852130d09b8954c103fa04dc78dd6897004ef1
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-09-20 16:39:23 +0800

    solutions for Offer 40 getLeastNumbers.java

--- Offer 40 getLeastNumbers.java
+++ Offer 40 getLeastNumbers.java
@@ -1,57 +1,12 @@
-class Solution1 {
+class Solution {
     public int[] getLeastNumbers(int[] arr, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        int[] ans = new int[k];
+        Arrays.sort(arr);
 
-        for(int num: arr)
-        {
-            pq.offer(num);
-
-            if(pq.size() > k)
-                pq.remove();
+        for(int i = 0; i < k; i++) {
+            ans[i] = arr[i];
         }
 
-        int[] res = new int[k];
-        int i = 0;
-        while(!pq.isEmpty())
-            res[i++] = pq.remove();
-
-        return res;
-    }
-}
-
-class Solution2 {
-    public int[] getLeastNumbers(int[] arr, int k) {
-        quickSort(arr, 0, arr.length - 1);
-
-        return Arrays.copyOf(arr, k);
-    }
-
-    public void quickSort(int[] arr, int l, int r)
-    {
-        if(l >= r)
-            return;
-
-        int i = l, j = r;
-        while(i < j)
-        {
-            while(i < j && arr[j] >= arr[l])
-                j--;
-            while(i < j && arr[i] <= arr[l])
-                i++;
-
-            swap(arr, i, j);
-        }
-
-        swap(arr, i, l);
-
-        quickSort(arr, l, i - 1);
-        quickSort(arr, i + 1, r);
-    }
-
-    public void swap(int[] arr, int i, int j)
-    {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        return ans;
     }
 }
