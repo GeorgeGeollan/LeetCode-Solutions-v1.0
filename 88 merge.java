e8a51688df4ba9cf71e98b6ba145c45884ae1a52
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-10-13 14:12:10 +0800

    solutions for 88 merge.java

--- 88 merge.java
+++ 88 merge.java
@@ -1,24 +1,23 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int ptr1 = m - 1;
-        int ptr2 = n - 1;
-        int tail = m + n - 1;
-        int cur;
+        int i = m - 1, j = n - 1, total = m + n - 1;
 
-        while(ptr1 >= 0 || ptr2 >= 0) {
-            if(ptr1 == -1)
-                cur = nums2[ptr2--];
+        while(i >= 0 || j >= 0) {
+            if(i < 0) {
+                nums1[total--] = nums2[j--];
+            }
 
-            else if(ptr2 == -1)
-                cur = nums1[ptr1--];
+            else if(j < 0) {
+                nums1[total--] = nums1[i--];
+            }
 
-            else if(nums1[ptr1] >= nums2[ptr2])
-                cur = nums1[ptr1--];
+            else if(nums1[i] > nums2[j]) {
+                nums1[total--] = nums1[i--];
+            }
 
-            else
-                cur = nums2[ptr2--];
-
-            nums1[tail--] = cur;
+            else {
+                nums1[total--] = nums2[j--];
+            }
         }
     }
 }
