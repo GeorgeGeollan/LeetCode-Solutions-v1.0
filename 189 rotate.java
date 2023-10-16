69068ab8af2aa06693a1c1449df35800b7d246e6
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-10-16 14:46:57 +0800

    solutions for 189 rotate.java

--- 189 rotate.java
+++ 189 rotate.java
@@ -1,33 +1,16 @@
-class Solution1 {
+class Solution {
     public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        int[] cpy = new int[n];
-
-        for(int i = 0; i < n; i++) {
-            cpy[(i + k) % n] = nums[i];
-        }
-
-        for(int i = 0; i < n; i++) {
-            nums[i] = cpy[i];
-        }
-    }
-}
-
-class Solution2 {
-    public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k % n;
-
-        reverse(nums, 0, n - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, n - 1);
+        k %= nums.length;
+        reverse(0, nums.length - 1, nums);
+        reverse(0, k - 1, nums);
+        reverse(k, nums.length - 1, nums);
     }
 
-    public void reverse(int[] nums, int start, int end) {
+    public void reverse(int start, int end, int[] arr) {
         while(start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
             start++;
             end--;
         }
