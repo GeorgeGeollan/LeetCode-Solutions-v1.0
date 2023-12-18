8bdd79970831d4169cc694f4acddbd6a64d57e28
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2023-12-18 11:37:33 +0800

    Update 6 convert.java

--- 6 convert.java
+++ 6 convert.java
@@ -1,27 +1,25 @@
 class Solution {
     public String convert(String s, int numRows) {
-        if(numRows < 2)
-            return s;
+        if(numRows == 1) return s;
 
-        List<StringBuilder> row = new ArrayList<>();
-
-        for(int i = 0; i < numRows; i++)
-            row.add(new StringBuilder());
-
-        int i = 0, flag = -1;
-
-        for(char ch: s.toCharArray()) {
-            row.get(i).append(ch);
-
-            if(i == 0 || i == numRows - 1)
-                flag = -flag;
-
-            i += flag;
+        StringBuilder[] sbArray = new StringBuilder[numRows];
+        for(int i = 0; i < numRows; i++) {
+            sbArray[i] = new StringBuilder();
         }
 
+        boolean reverse = false;
+        int ptr = 0;
         String ans = "";
 
-        for(StringBuilder sb: row) {
+        for(int i = 0; i < s.length(); i++) {
+            if(ptr == numRows - 1) reverse = true;
+            if(ptr == 0) reverse = false;
+            sbArray[ptr].append(s.charAt(i));
+
+            ptr = reverse ? ptr - 1 : ptr + 1;
+        }
+
+        for(StringBuilder sb: sbArray) {
             ans += sb.toString();
         }
 
